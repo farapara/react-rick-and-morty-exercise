1a73a189b2448fa8636c2b54a4944988d4346b92
Evi Fara <faraparaskevi@gmail.com>
2021-07-25 21:36:01 +0200

    Create CharacterCard component and css file, import back to Characters.js

---
+++ src/components/CharacterCard.js
@@ -0,0 +1,10 @@
+import "./CharacterCard.css";
+
+export default function CharacterCard() {
+  return (
+    <li className="CharacterCard">
+      <img src="" alt="" />
+      <h3>Name</h3>
+    </li>
+  );
+}
--- src/components/Characters.js
+++ src/components/Characters.js
@@ -1,15 +1,13 @@
 import "./Characters.css";
 import Form from "./Form";
+import CharacterCard from "./CharacterCard";
 
 export default function Characters() {
   return (
     <section className="characters">
       <Form />
       <ul className="CharacterList">
-        <li className="CharacterCard">
-          <img src="" alt="" />
-          <h3>Name</h3>
-        </li>
+        <CharacterCard />
       </ul>
       <button>More</button>
     </section>
