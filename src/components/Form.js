ea0cc66b4f8f65d598abf622dd392e873961bf14
Evi Fara <faraparaskevi@gmail.com>
2021-07-25 21:28:58 +0200

    Add styling

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,14 +2,19 @@ import "./Form.css";
 
 export default function Form() {
   return (
-    <form>
-      <input type="text" name="characterName" id="characterName" />
-      <selection>
+    <form className="form">
+      <input
+        type="text"
+        placeholder="Name"
+        name="characterName"
+        id="characterName"
+      />
+      <select>
         <option>All</option>
         <option>Alive</option>
         <option>Dead</option>
         <option>Unkown</option>
-      </selection>
+      </select>
     </form>
   );
 }
