7ad1f2d6dff836394f7f6792b59eaec413285a6a
Evi Fara <faraparaskevi@gmail.com>
2021-07-25 23:53:06 +0200

    Add Route Logic

--- src/App.js
+++ src/App.js
@@ -3,15 +3,26 @@ import Header from "./components/Header";
 import Home from "./pages/Home";
 import Characters from "./pages/Characters";
 import SingleCharacter from "./pages/SingleCharacter.js";
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <main>
-        <Home />
-        <Characters />
-        <SingleCharacter />
+        <Switch>
+          <Route path="/characters/singlecharacters/:id">
+            <SingleCharacter />
+          </Route>
+
+          <Route path="/characters">
+            <Characters />
+          </Route>
+
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
       </main>
     </div>
   );
--- src/pages/Characters.js
+++ src/pages/Characters.js
@@ -1,13 +1,16 @@
 import "./Characters.css";
 import Form from "../components/Form.js";
 import CharacterCard from "../components/CharacterCard.js";
+import { Link } from "react-router-dom";
 
 export default function Characters() {
   return (
     <section className="characters">
       <Form />
       <ul className="CharacterList">
-        <CharacterCard />
+        <Link to="/characters/singleCharacter/:id">
+          <CharacterCard />
+        </Link>
       </ul>
       <button>More</button>
     </section>
